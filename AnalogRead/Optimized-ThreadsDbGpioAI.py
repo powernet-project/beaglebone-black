1df86d0df295889c61196e225eabbd44d223be90
Gustavo Vianna Cezar <gcezar@stanford.edu>
2017-10-19 16:23:35 -0700

    added more logging

--- AnalogRead/Optimized-ThreadsDbGpioAI.py
+++ AnalogRead/Optimized-ThreadsDbGpioAI.py
@@ -40,7 +40,7 @@ logger.setLevel(logging.DEBUG)
 handler = RotatingFileHandler('my_log.log', maxBytes=2000, backupCount=10)
 logger.addHandler(handler)
 
-# Sentry setup for additional error reporting via 3rd party cloud service  
+# Sentry setup for additional error reporting via 3rd party cloud service
 client = Client(SENTRY_DSN)
 
 # Initializing Firebase
@@ -48,24 +48,24 @@ pwr_firebase = fb(FB_API_BASE_URL + 'ApplianceTest09')
 
 # Initializing GPIOs:
 appliance_lst = ["PW1", "RA1", "AC1", "DR1", "RF1", "SE1"]
-gpio_map = {"PW1": "P8_9", "RA1": "P8_10", "AC1": "P8_11",
-            "DR1": "P8_12", "RF1": "P8_14", "SE1": "P8_15"}
+gpio_map = {"PW1": "P8_9", "RA1": "P8_10", "AC1": "P8_15",
+            "DR1": "P8_12", "RF1": "P8_14", "SE1": "P8_11"}
 
 for key in gpio_map:
     GPIO.setup(gpio_map[key], GPIO.OUT)
-    GPIO.output(gpio_map[key], GPIO.LOW)
+    GPIO.output(gpio_map[key], GPIO.HIGH)
 
 
 def analog_read(off_value):
     """
         Analog Reading
     """
-    logger('Analog read called')
+    # logger('Analog read called')
     capture = adc.Capture()
     capture.cap_delay = 50000
     capture.oscilloscope_init(adc.OFF_VALUES + off_value, N_SAMPLES)
     capture.start()
-    
+
     while not capture.oscilloscope_is_complete():
         False  # This is a dumb condition just to keep the loop running
 
@@ -79,25 +79,31 @@ def producer_ai(format_ai, q_ai):
     """
         Producer AI
     """
-    logger('Producer AI called')
+    logger.info('Producer AI called')
     while(True):
         dts = []  # date/time stamp for each start of analog read
-        
+
         dts.append(str(datetime.now()))
         ai0 = analog_read(format_ai[0])
-        
+
         dts.append(str(datetime.now()))
         ai1 = analog_read(format_ai[1])
-        
+
         dts.append(str(datetime.now()))
         ai2 = analog_read(format_ai[2])
-        
+
         temp_ai = zip(ai0, ai1, ai2)
         temp_queue = [temp_ai, dts]
 
-        logger('Adding AI to the queue')
-        q_ai.put(temp_queue)
-        
+        # logger('Adding AI to the queue')
+
+        try:
+            q_ai.put(temp_queue, True, 2)
+
+        except Exception as exc:
+            logger.exception(exc)
+            client.captureException()
+
         time.sleep(2)
 
 
@@ -123,16 +129,16 @@ def consumer_ai(q_ai):
     """
         Consumer AI
     """
-    logger('Consumer AI called')
+    logger.info('Consumer AI called')
     template = [
         {
-            "sensor_id": 1, 
+            "sensor_id": 1,
             "samples": []
         }, {
-            "sensor_id": 2, 
+            "sensor_id": 2,
             "samples": []
         }, {
-            "sensor_id": 3, 
+            "sensor_id": 3,
             "samples": []
         }
     ]
@@ -141,39 +147,44 @@ def consumer_ai(q_ai):
 
     while(True):
         if not q_ai.empty():
-    
-            temp_cons = q_ai.get()
-            temp_ai = temp_cons[0]
-            temp_date = temp_cons[1]
-            
-            i_rms = RMS(temp_ai[1:])
-            
-            # Adding analog reads, sID and Date to lists for db upload
-            d_fb[0].get("samples").append({"RMS": i_rms[0], "date_time": temp_date[0]})
-            d_fb[1].get("samples").append({"RMS": i_rms[1], "date_time": temp_date[1]})
-            d_fb[2].get("samples").append({"RMS": i_rms[2], "date_time": temp_date[2]})
-            
-            # Queue is done processing the element
-            q_ai.task_done()
-            
-            if len(d_fb[1]["samples"]) == 10:
-                try:
-                    # send the request to the powernet site instead of firebase
-                    r_post_rms = requests.post(PWRNET_API_BASE_URL + "rms/", json={'devices_json': d_fb})
-                    
-                    if r_post_rms.status_code == 201:
-                        logger.info("Request was successful")
-                    else:
-                        logger.exception("Request failed")
-                        r_post_rms.raise_for_status()
 
-                    d_fb[:]=[]
-                    d_fb = None
-                    d_fb = copy.deepcopy(template)
-            
-                except Exception as exc:
-                    logger.exception(exc)
-                    client.captureException()
+            try:
+                temp_cons = q_ai.get(True,2)
+                temp_ai = temp_cons[0]
+                temp_date = temp_cons[1]
+
+                i_rms = RMS(temp_ai[1:])
+
+                # Adding analog reads, sID and Date to lists for db upload
+                d_fb[0].get("samples").append({"RMS": i_rms[0], "date_time": temp_date[0]})
+                d_fb[1].get("samples").append({"RMS": i_rms[1], "date_time": temp_date[1]})
+                d_fb[2].get("samples").append({"RMS": i_rms[2], "date_time": temp_date[2]})
+
+                # Queue is done processing the element
+                q_ai.task_done()
+
+                if len(d_fb[1]["samples"]) == 10:
+                    try:
+                        # send the request to the powernet site instead of firebase
+                        r_post_rms = requests.post(PWRNET_API_BASE_URL + "rms/", json={'devices_json': d_fb}, timeout = 2)
+
+                        if r_post_rms.status_code == 201:
+                            logger.info("Request was successful")
+                        else:
+                            logger.exception("Request failed")
+                            r_post_rms.raise_for_status()
+
+                        d_fb[:]=[]
+                        d_fb = None
+                        d_fb = copy.deepcopy(template)
+
+                    except Exception as exc:
+                        logger.exception(exc)
+                        client.captureException()
+
+            except Exception as exc:
+                logger.exception(exc)
+                client.captureException()
 
 
 def relay_act(device, state):
@@ -188,6 +199,12 @@ def relay_act(device, state):
 
 def relay_th():
     """
+        Relay Status
+    """
+
+    logger.info('Relay Thread called')
+
+    """
         Appliances ID:
             id:1 ; Powerwall_1
             id:2 ; Powerwall_2
@@ -197,22 +214,35 @@ def relay_th():
             id:6 ; AC_2
     """
     app_orig_states = ["OFF", "OFF", "OFF", "OFF", "OFF", "OFF"]
+    app_new_status = ["OFF", "OFF", "OFF", "OFF", "OFF", "OFF"]
 
+    # FIXME Need to setup only one request and get status of all appliances
     while(True):
-        AC_1 = requests.get(PWRNET_API_BASE_URL + "device/5")
-        status_AC1 = AC_1.json()["status"]
-        
-        SE_1 = requests.get(PWRNET_API_BASE_URL + "device/12")
-        status_SE1 = SE_1.json()["status"]
+        try:
+            AC_1 = requests.get(PWRNET_API_BASE_URL + "device/5", timeout = 2)
+            status_AC1 = AC_1.json()["status"]
+
+        except Exception as exc:
+            logger.exception(exc)
+            client.captureException()
+            status_AC1 = app_new_status[2]
+
+        try:
+            SE_1 = requests.get(PWRNET_API_BASE_URL + "device/12", timeout = 2)
+            status_SE1 = SE_1.json()["status"]
+
+        except Exception as exc:
+            logger.exception(exc)
+            client.captureException()
+            status_SE1 = app_new_status[5]
 
         app_new_status = ["OFF", "OFF", status_AC1, "OFF", "OFF", status_SE1]
-        
         for index, (first, second) in enumerate(zip(app_orig_states, app_new_status)):
             if first != second:
                 relay_act(appliance_lst[index], second)
                 app_orig_states = copy.deepcopy(app_new_status)
-                
-        time.sleep(1)
+
+        time.sleep(2)
 
 
 def main():
@@ -231,13 +261,13 @@ def main():
     format_ai = [i * 4 for i in range(n_ai)]
 
     # Initialize threads
-    producer_ai_thread = Thread(target=producer_ai, args=(format_ai, q_ai))
+    producer_ai_thread = Thread(name='Producer',target=producer_ai, args=(format_ai, q_ai))
     producer_ai_thread.start()
 
-    consumer_ai_thread = Thread(target=consumer_ai, args=(q_ai,))
+    consumer_ai_thread = Thread(name='Consumer',target=consumer_ai, args=(q_ai,))
     consumer_ai_thread.start()
 
-    relay_thread = Thread(target=relay_th)
+    relay_thread = Thread(name='Relay',target=relay_th)
     relay_thread.start()
 
 
